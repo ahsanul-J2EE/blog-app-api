a116759e750da6b47f9d7546b82905fc034684d6
Md Ahsanul Islam <ahsanul.islam@bjitacademy.com>
2023-05-15 18:12:10 +0600

    #Project set up.

---
+++ src/test/java/com/ahsanul/blogappapi/BlogAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahsanul.blogappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
