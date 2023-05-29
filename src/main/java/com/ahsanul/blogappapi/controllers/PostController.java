e17a7b7b44ca3e553b64e5609731a028e2812229
Md Ahsanul Islam <ahsanul.islam@bjitacademy.com>
2023-05-29 21:00:15 +0600

    added find post by user and find post by category.

--- src/main/java/com/ahsanul/blogappapi/controllers/PostController.java
+++ src/main/java/com/ahsanul/blogappapi/controllers/PostController.java
@@ -11,8 +11,10 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("api/posts")
+@RequestMapping("/api")
 public class PostController {
 
     @Autowired
@@ -26,8 +28,25 @@ public class PostController {
 
         return new ResponseEntity<PostDto>(createPost, HttpStatus.CREATED);
 
+    }
 
+    //get by user
 
+    @GetMapping("user/{userId}/posts")
+    public ResponseEntity<List<PostDto>> getPostsByUser(@PathVariable Integer userId){
+
+        List<PostDto> posts = this.postService.getAllPostsByUser(userId);
+
+        return new ResponseEntity<List<PostDto>>(posts, HttpStatus.OK);
+
+    }
+
+    @GetMapping("category/{categoryId}/posts")
+    public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable Integer categoryId){
+
+        List<PostDto> posts = this.postService.getAllPostsByCategory(categoryId);
+
+        return new ResponseEntity<List<PostDto>>(posts, HttpStatus.OK);
 
     }
 }
--- src/main/java/com/ahsanul/blogappapi/services/impl/PostServiceImpl.java
+++ src/main/java/com/ahsanul/blogappapi/services/impl/PostServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -76,12 +77,26 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public List<PostDto> getAllPostsByCategory(Integer categoryId) {
-        return null;
+        Category category = this.categoryRepo.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "Id", categoryId));
+
+        List<Post> posts = this.postRepo.findByCategory(category);
+
+        List<PostDto> postDtos = posts.stream().map((post) -> this.modelMapper.map(post,PostDto.class)).collect(Collectors.toList());
+
+        return postDtos;
     }
 
     @Override
     public List<PostDto> getAllPostsByUser(Integer userId) {
-        return null;
+        User user = this.userRepo.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "Id", userId));
+
+        List<Post> posts = this.postRepo.findByUser(user);
+
+        List<PostDto> postDtos = posts.stream().map((post) -> this.modelMapper.map(post,PostDto.class)).collect(Collectors.toList());
+
+        return postDtos;
     }
 
     @Override
